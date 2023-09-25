269c40da202eea24c855b5db7bef08f4d61e9066
Andre Felipe <afelipes@gmail.com>
2023-09-25 19:08:03 -0300

    conect app to api & insert input to search

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,17 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import {environment} from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import {PokemonData} from '../models/pokemonData'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
-  private baseURL:string = "";
+  private baseURL:string = ""
+  private pokeData:PokemonData | any
 
-  constructor() {
+  constructor( private http: HttpClient ) {
     this.baseURL = environment.pokeApi;
   }
 
-  getPokemon(pokemonName: string) {
-    console.log(this.baseURL);
+  getPokemon(pokemonName: string):Observable<PokemonData>{
+    this.pokeData = this
+                    .http
+                    .get<PokemonData>
+                    (`${this.baseURL}${pokemonName}`)
+    return this.pokeData;
   };
 }
